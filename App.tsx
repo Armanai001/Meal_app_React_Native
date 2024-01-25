6876616d7e75a96db10134c414179bdbd368aab8
Arman <theaiemperor@gmail.com>
2024-01-25 15:41:12 +0530

    Adding navigation for first screen

--- App.tsx
+++ App.tsx
@@ -1,21 +1,19 @@
-import {SafeAreaView, StyleSheet, View} from "react-native";
 import CategoryScreen from "./Screens/CategoryScreen";
-import {StatusBar} from "expo-status-bar";
+import {NavigationContainer} from "@react-navigation/native";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
 
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
     return (
-        <View style={styles.container}>
-            <SafeAreaView>
-                <StatusBar style='light'/>
-                <CategoryScreen/>
-            </SafeAreaView>
-        </View>
+        <>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name="Meal's Categories" component={CategoryScreen}/>
+                </Stack.Navigator>
+            </NavigationContainer>
+        </>
+
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    }
-})
