543e56561556d128cecc8b1b90831636b58f2559
Arman <theaiemperor@gmail.com>
2024-01-28 14:56:55 +0530

    Showing Favourite meals

--- Screens/FavouriteScreen.tsx
+++ Screens/FavouriteScreen.tsx
@@ -1,6 +1,9 @@
-import {StyleSheet, Text, View} from "react-native";
+import {ScrollView, StyleSheet, Text, View} from "react-native";
+import {MEALS} from "../data/data";
+import {useSelector} from "react-redux";
+import MealItem from "../Components/MealItem";
 
-export default function FavouriteScreen(){
+const EmptyView = () => {
     return <View style={styles.emptyContainer}>
         <Text style={styles.title}>
             You have no favorite meals yet.
@@ -8,17 +11,42 @@ export default function FavouriteScreen(){
     </View>
 }
 
+
+export default function FavouriteScreen() {
+    const favouriteMealsIds = useSelector((state: { favouriteMeals: { ids: string[] } }) => state.favouriteMeals.ids)
+    const favouriteMeal = MEALS.filter(item => favouriteMealsIds.includes(item.id))
+
+
+    return <>
+        {
+            favouriteMeal.length === 0 ? <EmptyView/> : <ScrollView>
+                {
+                    favouriteMeal.map(item => <View style={styles.container}>
+                            <MealItem key={item.id} data={item}/>
+                        </View>
+                    )
+                }
+            </ScrollView>
+        }
+    </>
+}
+
+
 const styles = StyleSheet.create({
     emptyContainer: {
-        flex:1,
+        flex: 1,
         justifyContent: 'center',
     },
-    title:{
-        color:'white',
-        padding:5,
-        margin:5,
-        fontSize:23,
+    title: {
+        color: 'white',
+        padding: 5,
+        margin: 5,
+        fontSize: 23,
         fontWeight: 'bold',
         textAlign: 'center',
+    },
+    container: {
+        flex: 1,
+        margin: 20
     }
 })
